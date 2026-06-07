4701818048ca3b0c677bae892e30a72a09887f9b
Joshijay1917 <jayjoshi1912007@gmail.com>
2026-06-07 22:04:36 +0530

    feat: analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -58,6 +59,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
     >
+      <head>
+        <Analytics />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
